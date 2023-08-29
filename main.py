369f993aad12ec136c73355e09766fda45724c77
toaster20913 <110645109+toaster20913@users.noreply.github.com>
2023-08-29 00:15:23 +0000

    3rd Iteration(Different Type of Questions)

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from tkinter import messagebox
 # Create the main application window
 app = tk.Tk()
 app.title("Plastic Pollution Quiz")
-#This is the size of the window that the game is played on
-app.geometry("400x200")
-#This is the background colour of the game
+# This is the size of the window that the game is played on
+app.geometry("400x300")
+# This is the background colour of the game
 app.configure(bg="blue")
 
 # Define the quiz questions and multiple-choice options
@@ -26,20 +26,28 @@ questions = [
         "options": ["Fish", "Dolphins", "Sea turtles, seabirds, and marine mammals", "Sharks"],
         "correct": "Sea turtles, seabirds, and marine mammals"
     },
-    # Add more questions here
- {
-    "question": "Question 4: What is the long-term environmental impact of plastic pollution on marine ecosystems?",
-    "options": ["Habitat destruction, harm to marine life", "Global Warming", "World Hunger", "Earthquakes"],
-    "correct": "Habitat destruction, harm to marine life"
-},
-  {
-    "question":"What is the primary source of plastic pollution in our oceans?",
-    "options":["Industrial Waste", "Natural debris", "Household Garbage", "Space debris"],
-    "correct": "Household Garbage"
+    {
+        "question": "Question 4: What is the long-term environmental impact of plastic pollution on marine ecosystems?",
+        "options": ["Habitat destruction, harm to marine life", "Global Warming", "World Hunger", "Earthquakes"],
+        "correct": "Habitat destruction, harm to marine life"
+    },
+    {
+        "question": "Question 5: What is the primary source of plastic pollution in our oceans?",
+        "options": ["Industrial Waste", "Natural debris", "Household Garbage", "Space debris"],
+        "correct": "Household Garbage"
+    }
+]
 
-  }
-  
-
+# Additional easy questions about plastic pollution in the ocean that answers will be typed
+questions += [
+    {
+        "question": "Question 6: What is the most common type of plastic found in the ocean?",
+        "answer": "polyethylene"
+    },
+    {
+        "question": "Question 7: What is the term for the gradual breakdown of plastics into smaller particles?",
+        "answer": "plastic degradation"
+    }
 ]
 
 # Variables to track current question and score
@@ -50,19 +58,37 @@ score = 0
 def display_question():
     question_data = questions[current_question]
     question_label.config(text=question_data["question"])
-    for i, option in enumerate(question_data["options"]):
-        option_buttons[i].config(text=option, state=tk.NORMAL)
+
+    # Hide or show widgets based on question type
+    if "options" in question_data:
+        for i, option_button in enumerate(option_buttons):
+            option_button.config(text=question_data["options"][i])
+            option_button.pack()
+        typed_answer_entry.pack_forget()
+        typed_answer_button.pack_forget()
+    elif "answer" in question_data:
+        for option_button in option_buttons:
+            option_button.pack_forget()
+        typed_answer_entry.pack()
+        typed_answer_button.pack()
+
+    # Update the score label
     score_label.config(text=f"Score: {score}")
 
 # Function to check the answer and move to the next question
-def check_answer(selected_option):
+def check_answer(selected_option=None, typed_answer=None):
     global current_question, score
     question_data = questions[current_question]
-    user_answer = question_data["options"][selected_option]
-    correct_answer = question_data["correct"]
 
-    if user_answer == correct_answer:
-        score += 1
+    if selected_option is not None:
+        user_answer = question_data["options"][selected_option]
+        correct_answer = question_data["correct"]
+        if user_answer == correct_answer:
+            score += 1
+    elif typed_answer is not None:
+        correct_answer = question_data["answer"]
+        if typed_answer.lower() == correct_answer.lower():
+            score += 1
 
     current_question += 1
 
@@ -83,8 +109,12 @@ question_label.pack(pady=20)
 option_buttons = []
 for i in range(4):
     option_button = tk.Button(app, text="", width=40, command=lambda selected_option=i: check_answer(selected_option))
-    option_button.pack(pady=5)
     option_buttons.append(option_button)
+
+typed_answer_entry = tk.Entry(app, width=40)
+
+typed_answer_button = tk.Button(app, text="Submit Typed Answer", width=40,
+                                command=lambda: check_answer(typed_answer=typed_answer_entry.get()))
 
 score_label = tk.Label(app, text="Score: 0", bg="blue", fg="white", font=("Arial", 12))
 score_label.pack(pady=10)
@@ -92,9 +122,5 @@ score_label.pack(pady=10)
 # Start the quiz
 display_question()
 
- 
 # Run the Tkinter main loop
 app.mainloop()
-
-
-
